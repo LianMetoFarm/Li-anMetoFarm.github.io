462c19f9b267dffcf1e1fd5ed70300f2c70845e2
LianMetoFarm <157159003+LianMetoFarm@users.noreply.github.com>
2024-02-21 06:47:11 +0800

    logout
    menambahkan file logout.js

---
+++ input_recording/logout.js
@@ -0,0 +1,8 @@
+// logout.js
+document.addEventListener("DOMContentLoaded", function() {
+    var logoutButton = document.querySelector('.btn-danger');
+  
+    logoutButton.addEventListener('click', function() {
+      window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
+    });
+  });
