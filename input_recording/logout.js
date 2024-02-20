652c537cc07a4a30794e0e3c0a67c1cc3f6145a6
LianMetoFarm <157159003+LianMetoFarm@users.noreply.github.com>
2024-02-21 07:55:16 +0800

    Update logout.js

--- input_recording/logout.js
+++ input_recording/logout.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var logoutButton = document.querySelector('.logout-btn');
     var logoutConfirmButton = document.getElementById('logoutConfirmBtn');
+    var cancelButton = document.querySelector('[data-bs-dismiss="modal"]'); // Menemukan tombol Batal pada modal
 
     logoutButton.addEventListener('click', function() {
       // Tampilkan modal konfirmasi
@@ -12,4 +13,11 @@ document.addEventListener("DOMContentLoaded", function() {
       // Redirect pengguna ke halaman logout jika konfirmasi diberikan
       window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
     });
-  });
+
+    // Tambahkan event listener untuk tombol Batal pada modal
+    cancelButton.addEventListener('click', function() {
+      // Tutup modal ketika tombol Batal ditekan
+      var myModal = new bootstrap.Modal(document.getElementById('logoutModal'));
+      myModal.hide();
+    });
+});
