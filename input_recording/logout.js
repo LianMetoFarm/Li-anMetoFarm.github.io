b57e8dd95c51303f0ee6f280ba2561b01ce95e32
LianMetoFarm <157159003+LianMetoFarm@users.noreply.github.com>
2024-02-21 06:51:05 +0800

    menambagkan if
    menambahkan if pakah anda ingin keluar

--- input_recording/logout.js
+++ input_recording/logout.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     var logoutButton = document.querySelector('.btn-danger');
   
     logoutButton.addEventListener('click', function() {
-      window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
+      // Menampilkan konfirmasi sebelum logout
+      var confirmation = confirm("Apakah Anda yakin ingin logout?");
+      if (confirmation) {
+        // Jika pengguna mengonfirmasi, maka arahkan ke halaman logout
+        window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
+      }
+      // Jika tidak, biarkan pengguna tetap di halaman saat ini
     });
   });
