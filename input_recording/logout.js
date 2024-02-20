1453f8191dfda1104eb744a077f0b64199b6bd9d
LianMetoFarm <157159003+LianMetoFarm@users.noreply.github.com>
2024-02-21 07:00:56 +0800

    update logoutn
    update logout

--- input_recording/logout.js
+++ input_recording/logout.js
@@ -1,14 +1,16 @@
 // logout.js
 document.addEventListener("DOMContentLoaded", function() {
     var logoutButton = document.querySelector('.btn-danger');
-  
+    var logoutConfirmButton = document.getElementById('logoutConfirmBtn');
+
     logoutButton.addEventListener('click', function() {
-      // Menampilkan konfirmasi sebelum logout
-      var confirmation = confirm("Apakah Anda yakin ingin logout?");
-      if (confirmation) {
-        // Jika pengguna mengonfirmasi, maka arahkan ke halaman logout
-        window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
-      }
-      // Jika tidak, biarkan pengguna tetap di halaman saat ini
+        // Tampilkan modal konfirmasi
+        var myModal = new bootstrap.Modal(document.getElementById('logoutModal'));
+        myModal.show();
     });
-  });
+
+    logoutConfirmButton.addEventListener('click', function() {
+        // Redirect pengguna ke halaman logout jika konfirmasi diberikan
+        window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
+    });
+});
