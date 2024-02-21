933e1a8cdfcd16f692482da6fe39b3eca6b38967
LianMetoFarm <157159003+LianMetoFarm@users.noreply.github.com>
2024-02-21 08:41:51 +0800

    update refres

--- input_recording/logout.js
+++ input_recording/logout.js
@@ -3,41 +3,42 @@ document.addEventListener("DOMContentLoaded", function() {
     var logoutConfirmButton = document.getElementById('logoutConfirmBtn');
     var cancelButton = document.querySelector('[data-bs-dismiss="modal"]'); // Menemukan tombol Batal pada modal
     var myModal = new bootstrap.Modal(document.getElementById('logoutModal'));
-    var refreshButton = document.querySelector('.refresh-btn'); //refres
-
+    
     logoutButton.addEventListener('click', function() {
-      // Tampilkan modal konfirmasi
-      myModal.show();
+        // Tampilkan modal konfirmasi
+        myModal.show();
     });
-
+    
     logoutConfirmButton.addEventListener('click', function() {
-      // Redirect pengguna ke halaman logout jika konfirmasi diberikan
-      window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
+        // Redirect pengguna ke halaman logout jika konfirmasi diberikan
+        window.location.href = 'https://lianmetofarm.github.io/profile/index.html';
     });
-
+    
     // Tambahkan event listener untuk tombol Batal pada modal
     cancelButton.addEventListener('click', function() {
-      // Tutup modal ketika tombol Batal ditekan
-      myModal.hide();
-      // Hapus backdrop
-      var backdrop = document.getElementsByClassName('modal-backdrop')[0];
-      backdrop.parentNode.removeChild(backdrop);
-      // Fokus kembali ke tombol logout setelah modal ditutup
-      logoutButton.focus();
+        // Tutup modal ketika tombol Batal ditekan
+        myModal.hide();
+        // Hapus backdrop
+        var backdrop = document.getElementsByClassName('modal-backdrop')[0];
+        backdrop.parentNode.removeChild(backdrop);
+        // Fokus kembali ke tombol logout setelah modal ditutup
+        logoutButton.focus();
     });
-
+    
+    
+    // Tambahkan event listener untuk menutup modal jika tekan tombol ESC
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            myModal.hide();
+            var backdrop = document.getElementsByClassName('modal-backdrop')[0];
+            backdrop.parentNode.removeChild(backdrop);
+            logoutButton.focus();
+        }
+    });
+    
+    var refreshButton = document.querySelector('.refresh-btn'); //refres
     refreshButton.addEventListener('click', function() {
         // Memuat ulang halaman saat tombol refresh ditekan
         window.location.reload();
-    });
-
-    // Tambahkan event listener untuk menutup modal jika tekan tombol ESC
-    document.addEventListener('keydown', function(event) {
-      if (event.key === 'Escape') {
-        myModal.hide();
-        var backdrop = document.getElementsByClassName('modal-backdrop')[0];
-        backdrop.parentNode.removeChild(backdrop);
-        logoutButton.focus();
-      }
     });
 });
