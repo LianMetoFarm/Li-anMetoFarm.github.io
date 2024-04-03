3ca8cbb352f680900bec6fc0c0540e24a8854e1d
LianMetoFarm <157159003+LianMetoFarm@users.noreply.github.com>
2024-04-04 05:45:20 +0800

    Update api.js

--- Dashboard/api.js
+++ Dashboard/api.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
                 var sheetId = sheet.properties.sheetId;
 
                 // Endpoint API untuk mendapatkan data dari sheet tertentu
-                var sheetUrl = baseUrl + spreadsheetId + '/values/' + sheetTitle + '!B10:B100,E10:E100?key=' + apiKey;
+                var sheetUrl = baseUrl + spreadsheetId + '/values/' + sheetTitle + '!B10:B100?key=' + apiKey;
 
                 // Ambil data dari sheet
                 $.get(sheetUrl, function(sheetData) {
