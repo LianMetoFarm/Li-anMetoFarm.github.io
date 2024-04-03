d18e163da5a7b7095e7abcfd188db4d3e55884fa
LianMetoFarm <157159003+LianMetoFarm@users.noreply.github.com>
2024-04-04 07:14:38 +0800

    Update api.js

--- Dashboard/api.js
+++ Dashboard/api.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
                         var rows = sheetData.values;
 
                         // Tambahkan data ke dalam tabel
-                        var table = $('<table>').addClass('table table-dark table-hover');
+                        var table = $('<table>').addClass('table table-dark table-striped table-hover');
                         // Tambahkan judul kolom
                         var headerRow = $('<tr>');
                         for (var k = 0; k < columnsToDisplay.length; k++) {
